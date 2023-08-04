9ab51b389aec13ae953e0af3b6e0d30086b6018b
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-04 16:08:34 +0300

    fix :

--- src/main/java/com/gassion/weather/repository/RoleRepository.java
+++ src/main/java/com/gassion/weather/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package com.gassion.weather.repository;
 
 import com.gassion.weather.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
