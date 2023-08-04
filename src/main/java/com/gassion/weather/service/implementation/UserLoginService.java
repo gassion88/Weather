0d5fe06db68f3d0d2a598681cac2aac6117f0b9d
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-05 00:43:07 +0300

    feat : add login service

---
+++ src/main/java/com/gassion/weather/service/implementation/UserLoginService.java
@@ -0,0 +1,41 @@
+package com.gassion.weather.service.implementation;
+
+import com.gassion.weather.entity.Role;
+import com.gassion.weather.entity.User;
+import com.gassion.weather.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserLoginService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserLoginService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user != null) {
+            return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                    user.getPassword(),
+                    mapRolesToAuthorities(user.getRoles()));
+        }else{
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+    }
+
+    private Collection< ? extends GrantedAuthority> mapRolesToAuthorities(Collection <Role> roles) {
+        Collection < ? extends GrantedAuthority> mapRoles = roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+        return mapRoles;
+    }
+}
