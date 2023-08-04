ed47a2a086b40e3e3640fdc1a3b0db83e5c88bef
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-05 01:01:04 +0300

    feat : add user dto

---
+++ src/main/java/com/gassion/weather/dto/UserDTO.java
@@ -0,0 +1,25 @@
+package com.gassion.weather.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UserDTO {
+    private Long id;
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
+    @NotEmpty(message = "Password should not be empty")
+    private String password;
+}
