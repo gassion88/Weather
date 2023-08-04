7f904e6cf46832d0f970051a7b4727183c7729f1
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-05 00:42:40 +0300

    feat : add controller

--- src/main/java/com/gassion/weather/controller/AuthController.java
+++ src/main/java/com/gassion/weather/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.gassion.weather.controller;
 
+import com.gassion.weather.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,5 +11,16 @@ public class AuthController {
     @GetMapping("/")
     public String showHopePage() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegisterPage(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
     }
 }
