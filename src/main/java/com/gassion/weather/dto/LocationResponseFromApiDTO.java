1452f2e8fe7178c67f11aaf0ee3328e4eeb0224e
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-08 19:41:16 +0300

    fix :

--- src/main/java/com/gassion/weather/dto/LocationResponseFromApiDTO.java
+++ src/main/java/com/gassion/weather/dto/LocationResponseFromApiDTO.java
@@ -2,7 +2,9 @@ package com.gassion.weather.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LocationResponseFromApiDTO {
     @JsonProperty("name")
