4a176472d80bff3abb012620e4a72de3bf563545
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-08 19:36:06 +0300

    feat :

---
+++ src/main/java/com/gassion/weather/dto/LocationResponseFromApiDTO.java
@@ -0,0 +1,19 @@
+package com.gassion.weather.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LocationResponseFromApiDTO {
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("lat")
+    private Double latitude;
+
+    @JsonProperty("lon")
+    private Double longitude;
+
+    @JsonProperty("country")
+    private String country;
+}
