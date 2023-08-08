05809927c1a989d45faf1c04b191f2c2af8414a8
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-08 19:03:16 +0300

    fix :

--- src/main/java/com/gassion/weather/controller/SearchController.java
+++ src/main/java/com/gassion/weather/controller/SearchController.java
@@ -1,14 +1,21 @@
 package com.gassion.weather.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Controller
 @RequestMapping("/search")
 public class SearchController {
     @GetMapping
-    public String showSearchPage() {
+    public String showSearchResult(@ModelAttribute("startString") String startString, Model model) {
+        model.addAttribute("city", Collections.EMPTY_LIST);
         return "search";
     }
 }
