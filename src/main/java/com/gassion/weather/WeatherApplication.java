f23b6178faf9b4cd08c3f4a2b0d264d16f8c07e6
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-12 17:43:35 +0300

    feat : frontend

--- src/main/java/com/gassion/weather/WeatherApplication.java
+++ src/main/java/com/gassion/weather/WeatherApplication.java
@@ -3,14 +3,21 @@ package com.gassion.weather;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.net.http.HttpClient;
 
 @SpringBootApplication
-public class WeatherApplication {
+public class WeatherApplication extends WebMvcConfigurationSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(WeatherApplication.class, args);
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }
