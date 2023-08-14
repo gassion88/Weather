41279936abe664fa1b4b0618c9dc619b3c572151
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-14 21:47:35 +0300

    refactor:

--- src/main/java/com/gassion/weather/controller/SearchController.java
+++ src/main/java/com/gassion/weather/controller/SearchController.java
@@ -1,7 +1,8 @@
 package com.gassion.weather.controller;
 
 import com.gassion.weather.dto.LocationResponseFromApiDTO;
-import com.gassion.weather.service.LocationLoaderService;
+import com.gassion.weather.entity.Location;
+import com.gassion.weather.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,17 +16,18 @@ import java.util.List;
 @Controller
 @RequestMapping("/search")
 public class SearchController {
-    private final LocationLoaderService locationLoaderService;
+    private final LocationService locationService;
 
     @Autowired
-    public SearchController(LocationLoaderService locationLoaderService) {
-        this.locationLoaderService = locationLoaderService;
+    public SearchController(LocationService locationService) {
+        this.locationService = locationService;
     }
 
     @GetMapping
     public String locationSearch(@ModelAttribute("startString") String startString, Model model) {
-        List<LocationResponseFromApiDTO> locations = locationLoaderService.loadByName(startString);
+        List<LocationResponseFromApiDTO> locations = locationService.loadByName(startString);
         model.addAttribute("locations", locations);
+        model.addAttribute("newLocation", new Location());
 
         return "search";
     }
