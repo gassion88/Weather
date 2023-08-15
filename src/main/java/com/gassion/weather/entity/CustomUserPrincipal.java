bea570cee83f71337554de3a151d8a6828d280c1
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-15 21:40:08 +0300

    feat : add new user principal

---
+++ src/main/java/com/gassion/weather/entity/CustomUserPrincipal.java
@@ -0,0 +1,52 @@
+package com.gassion.weather.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserPrincipal implements UserDetails {
+    private final User user;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserPrincipal(User user, Collection<? extends GrantedAuthority> authorities) {
+        this.user = user;
+        this.authorities = authorities;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
