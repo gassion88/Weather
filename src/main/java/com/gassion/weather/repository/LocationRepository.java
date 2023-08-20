4b0eb18a298f3bf62c4f6e2c4e796540bf1a254e
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-20 16:05:53 +0300

    feat : get user location repo

--- src/main/java/com/gassion/weather/repository/LocationRepository.java
+++ src/main/java/com/gassion/weather/repository/LocationRepository.java
@@ -5,9 +5,12 @@ import com.gassion.weather.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Integer> {
     Optional<Location> findByNameAndCountryCodeAndUserId(String name, String countryConde, Long userId);
+
+    Optional<List<Location>> findAllByUser(User user);
 }
