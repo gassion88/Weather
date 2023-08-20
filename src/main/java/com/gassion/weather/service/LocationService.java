d6390184715781fc560e8c6fefb447db9b7658ac
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-20 16:06:25 +0300

    feat :

--- src/main/java/com/gassion/weather/service/LocationService.java
+++ src/main/java/com/gassion/weather/service/LocationService.java
@@ -5,10 +5,12 @@ import com.gassion.weather.entity.Location;
 import com.gassion.weather.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LocationService {
     List<LocationResponseFromApiDTO> loadByName(String locationName);
     Location saveUserToLocation(Location newLocation, User user);
     void markSavedLocation(List<LocationResponseFromApiDTO> locations, long userId);
     void deleteLocationFromId(Integer id);
+    List<Location> getAllUserLocation(User user);
 }
