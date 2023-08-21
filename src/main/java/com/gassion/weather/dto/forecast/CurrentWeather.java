d0df885c79ec69ec942648882e73c945b15f7760
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-21 20:35:55 +0300

    feat : add dto

---
+++ src/main/java/com/gassion/weather/dto/forecast/CurrentWeather.java
@@ -0,0 +1,44 @@
+package com.gassion.weather.dto.forecast;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CurrentWeather {
+    @JsonProperty("temp")
+    private double temp;
+
+    @JsonProperty("name")
+    private String icon;
+
+    @JsonProperty("condition")
+    private String condition;
+
+    @JsonProperty("wind_speed")
+    private int windSpeed;
+
+    @JsonProperty("pressure_mm")
+    private double pressureMM;
+
+    @JsonProperty("humidity")
+    private double humidity;
+
+    @JsonProperty("daytime")
+    private String dayTime;
+
+    @JsonProperty("season")
+    private String season;
+
+    @JsonProperty("is_thunder")
+    private boolean isThunder;
+
+    @JsonProperty("prec_type")
+    private int precType;
+
+    @JsonProperty("cloudness")
+    private int cloudness;
+}
