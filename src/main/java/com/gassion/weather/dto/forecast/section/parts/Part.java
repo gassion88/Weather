b7c89c0e9308d5ea7231a593a3f36df8c4a810f0
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-23 20:22:45 +0300

    feat :

---
+++ src/main/java/com/gassion/weather/dto/forecast/section/parts/Part.java
@@ -0,0 +1,47 @@
+package com.gassion.weather.dto.forecast.section.parts;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Part {
+    @JsonProperty("temp_min")
+    private int tempMin;
+
+    @JsonProperty("temp_max")
+    private int tempMax;
+
+    @JsonProperty("temp_avg")
+    private int tempAvg;
+
+    @JsonProperty("feels_like")
+    private int feelsLike;
+
+    @JsonProperty("icon")
+    private String icon;
+
+    @JsonProperty("condition")
+    private String condition;
+
+    @JsonProperty("daytime")
+    private String dayTime;
+
+    @JsonProperty("polar")
+    private boolean polar;
+
+    @JsonProperty("wind_speed")
+    private int windSpeed;
+
+    @JsonProperty("pressure_mm")
+    private int pressureMM;
+
+    @JsonProperty("humidity")
+    private int humidity;
+
+    @JsonProperty("cloudness")
+    private int cloudness;
+}
