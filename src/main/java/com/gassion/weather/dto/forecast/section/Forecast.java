61bf444f121a16c5bb58686074b9f8a86cf55a10
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-25 18:49:00 +0300

    refactor : removed extra fields

--- src/main/java/com/gassion/weather/dto/forecast/section/Forecast.java
+++ src/main/java/com/gassion/weather/dto/forecast/section/Forecast.java
@@ -21,39 +21,8 @@ public class Forecast {
     private String sunset;
 
     @JsonProperty("moon_code")
-    private String moonPhase;
+    private int moonPhase;
 
     @JsonProperty("parts")
     private PartForecast partForecast;
-
-    @JsonProperty("tempAvg")
-    private double tempAvg;
-
-    @JsonProperty("iconCode")
-    private String iconCode;
-
-    @JsonProperty("condition")
-    private String condition;
-
-    @JsonProperty("wind_speed")
-    private int windSpeed;
-
-    @JsonProperty("pressure_mm")
-    private double pressureMM;
-
-    @JsonProperty("humidity")
-    private double humidity;
-
-    @JsonProperty("prec_prob")
-    private int precProb;
-
-    @JsonProperty("prec_type")
-    private int precType;
-
-    @JsonProperty("cloudness")
-    private double cloudness;
-
-    @JsonProperty("hour")
-    private String hour;
-
 }
