9c781326efc5a32d13ccce26301dbd080f3c0ca7
Soslan Gassiev <soslangassity04@gmail.com>
2023-08-25 18:49:33 +0300

    feat : add class for hourly forecast

---
+++ src/main/java/com/gassion/weather/dto/forecast/section/parts/Hour.java
@@ -0,0 +1,38 @@
+package com.gassion.weather.dto.forecast.section.parts;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Hour {
+    @JsonProperty("hour")
+    private String hour;
+
+    @JsonProperty("temp")
+    private int temp;
+
+    @JsonProperty("icon")
+    private String icon;
+
+    @JsonProperty("condition")
+    private String condition;
+
+    @JsonProperty("wind_speed")
+    private double windSpeed;
+
+    @JsonProperty("pressure_mm")
+    private int pressureMM;
+
+    @JsonProperty("humidity")
+    private int humidity;
+
+    @JsonProperty("is_thunder")
+    private boolean isThunder;
+
+    @JsonProperty("cloudness")
+    private double cloudness;
+}
