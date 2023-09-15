de8d1c0be2d1992e9d47a166426dbd904d6253b3
Soslan Gassiev <soslangassity04@gmail.com>
2023-09-15 12:53:40 +0300

    refactor :

--- src/main/java/com/gassion/weather/controller/ForecastController.java
+++ src/main/java/com/gassion/weather/controller/ForecastController.java
@@ -1,12 +1,14 @@
 package com.gassion.weather.controller;
 
 import com.gassion.weather.dto.CurrentWeatherDTO;
+import com.gassion.weather.dto.ForecastApiResponseDTO;
 import com.gassion.weather.dto.yandex_api.forecast.YandexApiForecastApiResponseDTO;
 import com.gassion.weather.dto.ToDayForecastDTO;
 import com.gassion.weather.entity.Location;
 import com.gassion.weather.service.ForecastService;
 import com.gassion.weather.service.LocationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class ForecastController {
 
+    @Qualifier("openWeatherApiForecastServiceImpl")
     private final ForecastService forecastService;
 
     private final LocationService locationService;
@@ -26,12 +29,12 @@ public class ForecastController {
     @GetMapping("/{location_id}")
     public String loadLocationForecast(@PathVariable int location_id, Model model) {
         Location location = locationService.getById(location_id, null);
-        YandexApiForecastApiResponseDTO yandexApiForecastApiResponseDTO = forecastService.loadForecastByCoordinates(
+        ForecastApiResponseDTO forecastApiResponseDTO = forecastService.loadForecastByCoordinates(
                 location.getLatitude().toString(),
                 location.getLongitude().toString());
 
-        CurrentWeatherDTO currentWeather = forecastService.getCurrentWeather(yandexApiForecastApiResponseDTO, location.getName());
-        ToDayForecastDTO toDayForecastDTO = forecastService.getToDayForecast(yandexApiForecastApiResponseDTO);
+        CurrentWeatherDTO currentWeather = forecastService.getCurrentWeather(forecastApiResponseDTO, location.getName());
+        ToDayForecastDTO toDayForecastDTO = forecastService.getToDayForecast(forecastApiResponseDTO);
 
         model.addAttribute("current", currentWeather);
         model.addAttribute("forecasts", toDayForecastDTO);
